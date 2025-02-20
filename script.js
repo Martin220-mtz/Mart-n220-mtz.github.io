204657fe83ae5ad97a56e482ab237f6ee57456c6
Martin220-mtz <axelmartinezes199@gmail.com>
2025-02-19 23:34:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function mostrarSeccion(numero) {
+    // Oculta todas las secciones
+    let secciones = document.querySelectorAll('.seccion');
+    secciones.forEach(seccion => seccion.style.display = 'none');
+
+    // Muestra solo la sección seleccionada
+    document.getElementById(`seccion${numero}`).style.display = 'block';
+}
+
+// Muestra la primera sección al cargar la página
+document.addEventListener("DOMContentLoaded", function() {
+    mostrarSeccion(1);
+});
